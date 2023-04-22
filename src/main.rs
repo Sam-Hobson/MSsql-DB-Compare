dfaad1c162d12aded9d1ca4e795450197b13ac4f
Sam Hobson <shhobson@protonmail.com>
2023-04-23 00:55:45 +1000

    Added quanit boiler plate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use quaint::{prelude::*, single::Quaint};
+
+#[tokio::main]
+async fn main() -> Result<(), quaint::error::Error> {
+    let conn = Quaint::new("").await?;
+    let result = conn.select(Select::default().value(1)).await?;
+
+    assert_eq!(
+        Some(1),
+        result.into_iter().nth(0).and_then(|row| row[0].as_i64()),
+    );
+
+    Ok(())
 }
