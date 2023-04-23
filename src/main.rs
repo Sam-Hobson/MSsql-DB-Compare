d57dc102aa493a44e738b688592d7c85f77ac0b2
Sam Hobson <shhobson@protonmail.com>
2023-04-23 23:17:29 +1000

    Added tiberius boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
-use quaint::{prelude::*, single::Quaint};
+use tiberius::{error::Error, AuthMethod, Client, Config};
+use tokio::net::TcpStream;
+use tokio_util::compat::TokioAsyncWriteCompatExt;
 
 #[tokio::main]
-async fn main() -> Result<(), quaint::error::Error> {
-    let conn = Quaint::new("").await?;
-    let result = conn.select(Select::default().value(1)).await?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut config = Config::new();
 
-    assert_eq!(
-        Some(1),
-        result.into_iter().nth(0).and_then(|row| row[0].as_i64()),
-    );
+    config.host("0.0.0.0");
+    config.port(1433);
+    config.authentication(AuthMethod::sql_server("sa", ""));
+
+    let tcp = TcpStream::connect(config.get_addr()).await?;
+    tcp.set_nodelay(true)?;
+
+    let client = match Client::connect(config, tcp.compat_write()).await {
+        // Connection successful.
+        Ok(client) => client,
+        // The server wants us to redirect to a different address
+        Err(Error::Routing { host, port }) => {
+            let mut config = Config::new();
+
+            config.host(&host);
+            config.port(port);
+            config.authentication(AuthMethod::sql_server("sa", ""));
+
+            let tcp = TcpStream::connect(config.get_addr()).await?;
+            tcp.set_nodelay(true)?;
+
+            // we should not have more than one redirect, so we'll short-circuit here.
+            Client::connect(config, tcp.compat_write()).await?
+        }
+        Err(e) => Err(e)?,
+    };
 
     Ok(())
 }
